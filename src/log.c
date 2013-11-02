ae6fcec44ecd6f7adfb608f5f895aaf35fda89b4
Fabian Mewes <architekt@coding4coffee.org>
2013-11-02 18:17:07 +0100

    Refactor log module

--- src/log.c
+++ src/log.c
@@ -2,63 +2,45 @@
 /*
  * vim:ts=4:sw=4:expandtab
  */
-#include <assert.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
 #include <stdarg.h>
-#include <string.h>
+#include <time.h>
 
 #include "log.h"
-#include "main.h"
-
-#define LOGBUF 4096
 
 static uint8_t log_level = LOG_DEBUG;
-static bool verbose = false;
 
-static void _logmsg(uint8_t level, bool print, const char *fmt, va_list args) {
-    FILE *stream = stdout;
+static const char *levelstr(uint8_t level) {
+    switch (level) {
+        case LOG_INFO:
+            return "INFO";
+        case LOG_WARN:
+            return "WARN";
+        case LOG_ERR:
+            return "ERROR";
+        case LOG_DEBUG:
+            return "DEBUG";
+        default:
+            return "";
+    };
+}
+
+void logmsg(uint8_t level, const char *fmt, ...) {
+    FILE *stream = (level == LOG_INFO) ? stdout : stderr;
+    va_list args;
+    char timestamp[64];
 
     if ((level & log_level) != log_level)
         return;
 
-    static char *levelstr;
-    if (0) {
-    } else if ((level & LOG_INFO) == LOG_INFO) {
-        levelstr = "INFO  - ";
-    } else if ((level & LOG_WARN) == LOG_WARN) {
-        levelstr = "WARN  - ";
-        stream = stderr;
-    } else if ((level & LOG_ERR) == LOG_ERR) {
-        levelstr = "ERROR - ";
-        stream = stderr;
-    } else if ((level & LOG_DEBUG) == LOG_DEBUG) {
-        levelstr = "DEBUG - ";
-        stream = stderr;
-    }
+    time_t now = time(NULL);
+    struct tm now_tm;
+    localtime_r(&now, &now_tm);
+    strftime(timestamp, sizeof(timestamp), "%x %X", &now_tm);
 
-    size_t levelstrlen = strlen(levelstr);
-
-    time_t t = time(NULL);
-    struct tm result;
-    struct tm *tmp = localtime_r(&t, &result);
-
-    char message[LOGBUF];
-    size_t len = strftime(message, sizeof(message), "%x %X ", tmp);
-
-    assert(LOGBUF > len + levelstrlen);
-    memcpy(message+len, levelstr, levelstrlen+1);
-
-    fprintf(stream, "%s", message);
-    vfprintf(stream, fmt, args);
-}
-
-void logmsg(uint8_t level, const char *fmt, ...) {
-    va_list args;
+    fprintf(stream, "%s %-5s - ", timestamp, levelstr(level));
     va_start(args, fmt);
-    _logmsg(level, verbose, fmt, args);
+    vfprintf(stream, fmt, args);
     va_end(args);
 }
